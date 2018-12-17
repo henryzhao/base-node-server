8d70fdcab770d43abd8610bd8d87fdf49cfa1b9f
Ubuntu <zhaoyuqi54@gmail.com>
2018-12-17 07:50:21 -0800

    update readme

--- routes/web/index.js
+++ routes/web/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LuLiangDev' });
+  res.render('index', { title: 'Base-Node-Server' });
 });
 
 module.exports = router;
